3668edd48d4dbadaac2583da11ada14343efcc69
gguer3 <gguer3@uic.edu>
2022-01-26 06:28:51 -0600

    testing background

--- sketch.js
+++ sketch.js
@@ -5,6 +5,8 @@ function setup() {
   //console.log("message");
 
   function draw() {
+    /*
+    //fill in background
     fill(240);
     rect(0, 0, windowWidth/2, windowHeight/2);
     fill(120);
@@ -13,6 +15,11 @@ function setup() {
     rect(0, windowHeight/2, windowWidth/2, windowHeight/2);
     fill(0);
     rect(windowWidth/2, windowHeight/2, windowWidth/2, windowHeight/2);
+
+    //draw curves
+    */
+
+    background(220);
     
 
 
