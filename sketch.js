c83fd83e4c21ef203b53f1b9cae9f62a4f136ce4
gguer3 <gguer3@uic.edu>
2022-01-26 06:51:48 -0600

    background test 2

--- sketch.js
+++ sketch.js
@@ -16,15 +16,28 @@ function setup() {
     fill(0);
     rect(windowWidth/2, windowHeight/2, windowWidth/2, windowHeight/2);
 
-    //draw curves
+    //DRAW CURVES
+    noFill(0);
+      //top left
+    stroke(0);
+    bezier(windowWidth/2, windowHeight/2, 0, 0, windowWidth/2, 0, windowWidth/2, windowHeight/2);
+    bezier(windowWidth/2, windowHeight/2, 0, 0, 0, windowHeight/2, windowWidth/2, windowHeight/2);
+      //top right
+    stroke(60);
+    bezier(windowWidth/2, windowHeight/2, windowWidth/2, 0, windowWidth, 0, windowWidth/2, windowHeight/2);
+    bezier(windowWidth/2, windowHeight/2, windowWidth, 0, windowWidth, windowHeight/2, windowWidth/2, windowHeight/2);
+      //bottom left
+    stroke(120);
+    bezier(windowWidth/2, windowHeight/2, 0, windowHeight/2, 0, windowHeight, windowWidth/2, windowHeight/2);
+    bezier(windowWidth/2, windowHeight/2, 0, windowHeight, windowWidth/2, windowHeight, windowWidth/2, windowHeight/2);
+      //bottom right
+    stroke(240);
+    bezier(windowWidth/2, windowHeight/2, windowWidth/2, windowHeight, windowWidth, windowHeight, windowWidth/2, windowHeight/2);
+    bezier(windowWidth/2, windowHeight/2, windowWidth, windowHeight/2, windowWidth, windowHeight, windowWidth/2, windowHeight/2);
+
     */
 
-    background(220);
+    background(225, 50, 200);
     
-
-
-
-
-
 
   }
