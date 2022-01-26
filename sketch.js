f434c586686f809151bcbb5b891e0a2b36c70b1c
gguer3 <gguer3@uic.edu>
2022-01-26 06:20:56 -0600

    testing

---
+++ sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+    createCanvas(windowWidth, windowHeigth);
+  }
+  
+  //console.log("message");
+
+  function draw() {
+    fill(240);
+    rect(0, 0, windowWidth/2, windowHeight/2);
+    fill(120);
+    rect(windowWidth/2, 0, windowWidth/2, windowHeight/2);
+    fill(60);
+    rect(0, windowHeight/2, windowWidth/2, windowHeight/2);
+    fill(0);
+    rect(windowWidth/2, windowHeight/2, windowWidth/2, windowHeight/2);
+    
+
+
+
+
+
+
+  }
